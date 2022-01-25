5e4fb9782f22314a39d94caf2dac8c264b7ab4bb
Lucho <luchi_m.022@hotmail.com>
2022-01-24 22:23:25 -0300

    internal modifier for some methods

--- data/src/main/java/com/luc/data/local/LocalDataSource.kt
+++ data/src/main/java/com/luc/data/local/LocalDataSource.kt
@@ -3,5 +3,10 @@ package com.luc.data.local
 import com.luc.common.entities.asUser
 
 class LocalDataSource(private val localDatabaseDAO: LocalDatabaseDAO) {
-    suspend fun getUser(id: String) = localDatabaseDAO.getUser(id).asUser()
+
+    /**
+     * Declare all methods with internal modifier
+     */
+
+    internal suspend fun getUser(id: String) = localDatabaseDAO.getUser(id).asUser()
 }
