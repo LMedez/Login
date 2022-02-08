b9aa0d9c1d37b2169b0d50ac105a7222c7ddf3d2
Lucho <luchi_m.022@hotmail.com>
2022-02-08 12:25:24 -0300

    Improvements for Room database

--- data/src/main/java/com/luc/data/local/dao/UserDao.kt
+++ data/src/main/java/com/luc/data/local/dao/UserDao.kt
@@ -3,9 +3,21 @@ package com.luc.data.local.dao
 import androidx.room.Dao
 import androidx.room.Query
 import com.luc.common.entities.UserEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 @Dao
 abstract class UserDao : BaseDao<UserEntity> {
     @Query("SELECT * FROM UserEntity WHERE id = :id")
-    abstract suspend fun getUser(id: String) : UserEntity
+    abstract suspend fun getUser(id: String): UserEntity
+
+    /**
+     * Get a user by id.
+     * @return the user from the table with a specific id.
+     */
+    @Query("SELECT * FROM UserEntity WHERE id = :id")
+    protected abstract fun getUserById(id: String): Flow<UserEntity>
+
+    fun getDistinctUserById(id: String):
+            Flow<UserEntity> = getUserById(id).distinctUntilChanged()
 }
